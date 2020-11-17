e44160aed86e503cad1019a3a6498841951a57c5
j-orellana <71954734+j-orellana@users.noreply.github.com>
2020-11-16 22:29:32 -0600

    Add files via upload

---
+++ nodos.h
@@ -0,0 +1,16 @@
+#ifndef NODOS_H
+#define NODOS_H
+#include<iostream>
+#include<string.h>
+using namespace std;
+class nodos
+{
+	public:
+		int id;
+		string nombre;
+		nodos *Iz;
+		nodos *De;
+	protected:
+};
+
+#endif
